27a4155521998b729407368dffeda837db4f5ae0
Max Wilson <wilson.max@gmail.com>
2021-03-26 11:20:35 -0700

    wip

--- arm/Program.fs
+++ arm/Program.fs
@@ -1,6 +1,13 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System
+open Farmer
+open Farmer.Builders
+
+let helloWorld = functions {
+    name "helloWorld"
+    storage_account_name "helloWorldData"    
+}
 
 // Define a function to construct a message to print
 let from whom =
